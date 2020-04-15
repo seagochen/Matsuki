428500787909ef8ed1461958870f65c7136572a3
Orlando Chen <seagochen@hotmail.com>
2020-04-15 14:04:57 +0800

    added new features to ArgsUsesRegularExpression

--- matsuki/argspattern/ArgsUsesRegularExpression.py
+++ matsuki/argspattern/ArgsUsesRegularExpression.py
@@ -1,78 +1,161 @@
 # -*- coding: utf-8 -*-
 # Author: Orlando Chen
 # Created: Feb 07, 2020
-# LastChg: Apr 07, 2020
+# LastChg: Apr 15, 2020
 
 import re
 from siki.basics import FileUtils
 from siki.basics import Exceptions
 from siki.basics import Convert
 
+class RegularToken(object):
 
-def use_args_regular_check(regDict: dict, args: dict):
-
-    finalDict = {}
-
-    # first, verify keys from second variable are matches to first variable
-    for key in args.keys():
-        if key not in args.keys():
-            raise Exceptions.NoAvailableResourcesFoundException("key: {} in args not matched to regDict".format(key))
-
-        # regular check
-        if re.match(regDict[key], args[key]) is not None:
-            finalDict[key] = args[key]
-        else:
-            finalDict[key] = None
-
-    # return to caller
-    return finalDict
+    def __init__(self, key: str, mapping: str, regular: str):
+        self.key = key
+        self.mapping = mapping
+        self.regular = regular
 
 
-def trim_tailer_from_text(text:str):
-    if text[-2] == '\r':
-        text = text.replace("\r\n", "")
-    else:
-        text = text.replace("\n", "")
+
+class ArgsMappingToken(object):
+    """
+    The token used to store the parsed data, including the key of the key-value pair, the mapped key, and the regular matching rules
+    """
+
+    def __init__(self):
+        self.tokens = []
+
     
-    return text
-
-
-
-def apply_reg_exp(rulefile: str, args: dict):
-    """
-    the format of rulefile:
-    [keyname] [regexpress]
-
-    @Args:
-    * [rulefile] str, path of file
-    * [args] dict, data contained with key and value
-
-    @Returns:
-    * [dict(str:obj)] dict type
-    """
-
-    if not FileUtils.exists(rulefile):
-        raise Exceptions.NoAvailableResourcesFoundException("file: {} not found".format(rulefile))
-
-    # rules dict
-    rules = {}
-
-    # read lines in file
-    for line in open(rulefile, "rt"):
-        # trim tailer
-        line = trim_tailer_from_text(line)
-
-        # split token from spaces
-        seps = line.split(' ')
+    def trim_tailer_from_text(self, text: str):
+        """
+        Windows and Linux systems have different line endings. These special symbols may cause regular matching to fail, so need to remove them all
+        """
+        if text[-2] == '\r':
+            text = text.replace("\r\n", "")
+        else:
+            text = text.replace("\n", "")
         
-        # check size
-        if len(seps) < 2 or len(seps) >= 3:
-            raise Exceptions.InvalidArithException("regular line: {} is broken".format(line))
+        return text
 
-        # append rules to dict
-        rules[seps[0]] = R"{}".format(seps[1])
 
-    return use_args_regular_check(rules, args)
+    def search_token(self, key):
+        """
+        from token list, search the matched token
+        """
+
+        for t in self.tokens:
+            if t.key == key:
+                return t
+            
+        return None
+
+
+    
+    def use_args_regular_check(self, tokenList: list, args: dict):
+        """
+        Regularly match the parameters, if the format is correct, the data of the parameter will be retained, if it is wrong, it will be discarded
+        """
+
+        finalDict = {}
+
+        # first, verify keys from second variable are matches to first variable
+        for key, val in args.items():
+            token = self.search_token(key)
+
+            if not token:
+                raise Exceptions.NoAvailableResourcesFoundException("key: {} in args not matched to token list".format(key))
+
+            # do regular check
+            if re.match(token.regular, val) is not None:
+                finalDict[token.mapping] = val
+            else:
+                finalDict[token.mapping] = None
+
+        # return to caller
+        return finalDict
+
+
+    
+    def clear_tokens(self):
+        """
+        cleaning up data caches
+        """
+        self.tokens.clear()
+
+    
+
+    def parsing_reg_file(self, ruleFile: str, args: dict):
+        """
+        Parse the configuration file, and then verify the validity of the input parameters
+
+        the format of rulefile:
+        [argument key] [mapping key] [regular expression]
+
+        @Args:
+        * [ruleFile] str, path of file
+        * [args] dict, data contained with key and value
+
+        @Returns:
+        * [dict(str:obj)] dict type
+        """
+
+        if not FileUtils.exists(rulefile):
+            raise Exceptions.NoAvailableResourcesFoundException("file: {} not found".format(rulefile))
+
+        # parse the file
+        for line in open(rulefile, "rt"):
+            # trim tailer
+            line = self.trim_tailer_from_text(line)
+
+            # split token from spaces
+            seps = line.split(' ')
+            
+            # check size
+            if len(seps) != 3:
+                raise Exceptions.InvalidArithException("regular line: {} is broken, [argument key] [mapping key] [regular expression]".format(line))
+
+            # append regular token to list
+            self.tokens.append(RegularToken(seps[0], seps[1], R"{}".format(seps[2])))
+
+        # After parsing the file, first extract the dictionary consisting of {old key-regular match} from the file, 
+        # and then perform regular filtering on the parameters
+        filtered_args = self.use_args_regular_check(self.tokens, args)
+
+        return filtered_args
+
+
+    def parsing_reg_rules(self, ruleList: list, args: dict):
+        """
+        Parse the configuration list, and then verify the validity of the input parameters
+
+        the format of rule list:
+        [argument key] [mapping key] [regular expression]
+
+        @Args:
+        * [ruleList] list
+        * [args] dict, data contained with key and value
+
+        @Returns:
+        * [dict(str:obj)] dict type
+        """
+
+        # parse the file
+        for line in ruleList:
+            # split token from spaces
+            seps = line.split(' ')
+            
+            # check size
+            if len(seps) != 3:
+                raise Exceptions.InvalidArithException("regular line: {} is broken, [argument key] [mapping key] [regular expression]".format(line))
+
+            # append regular token to list
+            self.tokens.append(RegularToken(seps[0], seps[1], seps[2]))
+
+        # After parsing the file, first extract the dictionary consisting of {old key-regular match} from the file, 
+        # and then perform regular filtering on the parameters
+        filtered_args = self.use_args_regular_check(self.tokens, args)
+
+        return filtered_args        
 
 
 
@@ -80,15 +163,23 @@ if __name__ == "__main__":
     args0 = {
         "key1": "seago@seagosoft.com",
         "key2": "0851-123456789",
-        "key3": "root/foo/bar.txt"
+        "key3": "123456789"
     }
 
-    print(apply_reg_exp("./rule.txt", args0))
+    rules = [
+        r"key1 to_key1 ^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$", # email
+        r"key2 to_key2 ^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$", # phone
+        r"key3 to_key3 ^[0-9]+$" # numbers
+    ]
+
+    amt = ArgsMappingToken()
+
+    print(amt.parsing_reg_rules(rules, args0))
 
     args1 = {
         "key1": "_seago^seagosoft.com",
         "key2": "0851-123456789a",
-        "key3": "root_foo_bar"
+        "key3": "123456789a"
     }
 
-    print(apply_reg_exp("./rule.txt", args1))
+    print(amt.parsing_reg_rules(rules, args1))
